e501242a53c5c0c9153ac51c5b51e8e27638fdba
Qboo-star <2842850273@qq.com>
2026-08-05 14:57:12 +0800

    初始化，上传Java练习代码,Hashset

---
+++ src/mylist/listDamo1.java
@@ -0,0 +1,14 @@
+package mylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class listDamo1 {
+    public static void main(String[] args) {
+        List<String> list=new ArrayList<>();
+
+        list.add("aaa");
+
+    }
+
+}
